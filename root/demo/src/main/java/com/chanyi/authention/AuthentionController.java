7b0cd422c5802d684e6e7592075af8ac30c90a5f
chanyi <791814697@qq.com>
2019-06-13 15:58:36 +0800

    fix bug

---
+++ root/demo/src/main/java/com/chanyi/authention/AuthentionController.java
@@ -0,0 +1,9 @@
+package com.chanyi.authention;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class AuthentionController extends WebSecurityConfigurerAdapter{
+	
+}
