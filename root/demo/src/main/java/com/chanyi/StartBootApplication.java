56165431862fa3d439275a29a1ba16857e62af62
lilei <791814697@qq.com>
2019-06-13 08:10:35 +0800

    add.add framework

---
+++ root/demo/src/main/java/com/chanyi/StartBootApplication.java
@@ -0,0 +1,14 @@
+package com.chanyi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * lilei
+ */
+@SpringBootApplication
+public class StartBootApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
