8c3102016bf9cd12a6690451d01317bd2d611cac
chanyi <791814697@qq.com>
2019-07-01 17:08:34 +0800

    add.gitignore

--- root/demo/src/main/java/com/chanyi/authention/WebSecurityConfigurer.java
+++ root/demo/src/main/java/com/chanyi/authention/WebSecurityConfigurer.java
@@ -25,13 +25,13 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 ////		.antMatchers("/test/test"); //拦截忽略
 //	}
 //
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-////		http.authorizeRequests().antMatchers("/signup", "/about").permitAll() // #4
-////				.antMatchers("/admin/**").hasRole("ADMIN") // “/admin/”开头的URL必须要是管理员用户，譬如”admin”用户
-////				.anyRequest().authenticated() // #7
-////				.and().formLogin()// 任何人(包括没有经过验证的)都可以访问”/login”和”/login?error”。permitAll()是指用户可以访问formLogin()相关的任何URL
-////				.loginPage("/login")//  注明了登陆页面，意味着用GET访问”/login”时，显示登陆页面
-////				.permitAll(); // #5
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/signup", "/about").permitAll() // #4
+				.antMatchers("/admin/**").hasRole("ADMIN") // “/admin/”开头的URL必须要是管理员用户，譬如”admin”用户
+				.anyRequest().authenticated() // #7
+				.and().formLogin()// 任何人(包括没有经过验证的)都可以访问”/login”和”/login?error”。permitAll()是指用户可以访问formLogin()相关的任何URL
+				.loginPage("/login/login")//  注明了登陆页面，意味着用GET访问”/login”时，显示登陆页面
+				.permitAll(); // #5
+	}
 }
