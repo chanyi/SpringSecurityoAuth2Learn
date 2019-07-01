bc83ffb46cfec4e3811f96ee40ea758fee15a81a
chanyi <791814697@qq.com>
2019-07-01 17:38:34 +0800

    add.gitignore

--- root/demo/src/main/java/com/chanyi/authention/WebSecurityConfigurer.java
+++ root/demo/src/main/java/com/chanyi/authention/WebSecurityConfigurer.java
@@ -31,7 +31,18 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 				.antMatchers("/admin/**").hasRole("ADMIN") // “/admin/”开头的URL必须要是管理员用户，譬如”admin”用户
 				.anyRequest().authenticated() // #7
 				.and().formLogin()// 任何人(包括没有经过验证的)都可以访问”/login”和”/login?error”。permitAll()是指用户可以访问formLogin()相关的任何URL
-				.loginPage("/login/login")//  注明了登陆页面，意味着用GET访问”/login”时，显示登陆页面
+				.loginPage("/login/login")//  注明了登陆页面，意味着用GET访问”/login/login”时，显示登陆页面(自定义页面)
 				.permitAll(); // #5
+		//安全头的配置
+		 http.headers()
+		 	.contentTypeOptions();
+		 http.headers()
+             .xssProtection();
+		 http.headers()
+             .cacheControl();
+		 http.headers()
+             .httpStrictTransportSecurity();
+		 http.headers()
+             .frameOptions();
 	}
 }
