b8870509e102cc2c3ceafc7cedef3efd58ff9401
lilei <791814697@qq.com>
2019-07-15 08:03:31 +0800

    add

--- root/demo/src/main/java/com/chanyi/controller/TestController.java
+++ root/demo/src/main/java/com/chanyi/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.chanyi.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,18 @@ public class TestController {
 	@GetMapping("/test")
 	public String test(){
 		return "this is test";
+	}
+
+	@ResponseBody
+	@GetMapping("/getCurrentUser")
+	public Object getCurrentUser(Authentication authentication){
+		return authentication;
+	}
+
+	@ResponseBody
+	@GetMapping("/getUserDetails")
+	public Object getUserDetails(@AuthenticationPrincipal UserDetails userDetails){
+		return userDetails;
 	}
 	
 	public static void main(String[] args){
---
+++ root/demo/src/main/java/com/chanyi/model/ImageCode.java
@@ -0,0 +1,65 @@
+package com.chanyi.model;
+
+
+import java.awt.image.BufferedImage;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/** */
+
+public class ImageCode {
+
+  /** */
+  private BufferedImage  image;
+
+  /**
+   *
+   */
+  private String code;
+
+  /**
+   *
+   */
+  private LocalDateTime expireTime;
+
+  public  ImageCode(BufferedImage image,String code,int expireSeconds){
+    this.image  = image;
+    this.code = code;
+    this.expireTime = LocalDateTime.now().plusSeconds(expireSeconds);
+
+  }
+
+  public BufferedImage getImage() {
+    return image;
+  }
+
+  public void setImage(BufferedImage image) {
+    this.image = image;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public LocalDateTime getExpireTime() {
+    return expireTime;
+  }
+
+  public void setExpireTime(LocalDateTime expireTime) {
+    this.expireTime = expireTime;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuffer sb = new StringBuffer("ImageCode{");
+    sb.append("image=").append(image);
+    sb.append(", code='").append(code).append('\'');
+    sb.append(", expireTime=").append(expireTime);
+    sb.append('}');
+    return sb.toString();
+  }
+}
